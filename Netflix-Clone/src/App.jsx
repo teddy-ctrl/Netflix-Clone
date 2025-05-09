baf430d3c194e712b9394577b9b99f69379d07bc
teddy-ctrl <tedu21@gmail.com>
2025-05-09 21:13:04 +0300

    footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './component/Header/Header'
+import Footer from './component/Footer/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }

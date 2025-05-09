0d63850706984ec2c9316914257e1c7133ddf487
teddy-ctrl <tedu21@gmail.com>
2025-05-09 17:57:44 +0300

    Header

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import './App.css'
+import Header from './component/Header/Header'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
---
+++ Netflix-Clone/src/component/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Logo from '../../assets/image/Logo/netfilix.png'
+import './header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+const Header = () => {
+  return (
+    <>
+      <div className="header-continer">
+        <div className="header-wraper">
+          <div className="header-link">
+            <ul>
+              <li><img src={Logo} alt="" /></li>
+              <li>Netflix</li>
+              <li>Home</li>
+              <li>TVShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Browse by Languages</li>
+            </ul>
+          </div>
+          <div className="header-icon-link">
+            <div><SearchIcon /></div>
+            <div><NotificationsNoneIcon /></div>
+            <div><AccountBoxIcon /></div>
+            <div><KeyboardArrowDownIcon /></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;

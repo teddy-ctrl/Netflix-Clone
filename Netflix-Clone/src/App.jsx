54b22436f33cc630f71f422da41d898cb0c27ae0
teddy-ctrl <tedu21@gmail.com>
2025-05-10 23:02:35 +0300

    banner

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './component/Header/Header'
 import Footer from './component/Footer/Footer'
+import Banner from './component/Banner/Banner'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   )
--- Netflix-Clone/src/utils/requests.jsx
+++ Netflix-Clone/src/utils/requests.jsx
@@ -3,7 +3,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover.tv?api_key=${API_KEY}&with_netorks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
